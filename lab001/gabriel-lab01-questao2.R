8a8ff2ff1646ba74d3ed4efbe5f33f9fe3418aeb
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2012-02-15 17:15:46 -0300

    remove useless print function from lab001q1

--- lab001/gabriel-lab01-questao2.R
+++ lab001/gabriel-lab01-questao2.R
@@ -1,7 +1,7 @@
 source("region-function.R")
 
 data = read.csv("salarios-ti-formatted.csv")
-
+data.with.region = cbind(data, Regiao = mapply(regionOf, data$UF))
 
 write.table(data.with.region, file="output-questao2.txt")
 

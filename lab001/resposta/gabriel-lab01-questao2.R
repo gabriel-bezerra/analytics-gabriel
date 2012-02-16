9f4135481b43c060e905c9aabba4f85ea14cbaa9
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2012-02-16 16:32:52 -0300

    move respostas para outro diretorio

--- lab001/resposta/gabriel-lab01-questao2.R
+++ lab001/resposta/gabriel-lab01-questao2.R
@@ -3,5 +3,8 @@ source("region-function.R")
 data = read.csv("salarios-ti-formatted.csv")
 data.with.region = cbind(data, Regiao = mapply(regionOf, data$UF))
 
-write.table(data.with.region, file="output-questao2.txt")
+aggregate(data.with.region, Regiao)
+#average.wage = data.frame()
+
+#write.table(data.with.region, file="output-questao2.txt")
 

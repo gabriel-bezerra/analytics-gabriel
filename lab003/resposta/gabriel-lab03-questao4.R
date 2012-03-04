7307ab280f66ae96600944147c78c347d4d26e51
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2012-03-04 15:32:21 -0300

    adiciona título, subtítulo ao gráfico das FDPs juntas; melhora espaçamento entre linhas do script

--- lab003/resposta/gabriel-lab03-questao4.R
+++ lab003/resposta/gabriel-lab03-questao4.R
@@ -4,7 +4,7 @@ args <- commandArgs(trailingOnly = TRUE)
 media <- as.numeric(args[1])
 desvio.padrao <- as.numeric(args[2])
 
-tamanho.da.amostra = 5000
+tamanho.da.amostra = 10000
 
 # Cria as amostra normal e a log-normal
 normal <- rnorm(n = tamanho.da.amostra, mean = media, sd = desvio.padrao)
@@ -13,15 +13,23 @@ log.normal <- exp(normal)
 # Plota as FDPs
 png(filename = "output-questao4-graficos-juntos.png", width = 960, height = 960)
 plot(density(normal), col = "red", lwd = 2, las = 1,
-     xlim = c(media - 4*desvio.padrao, media + 3*4*desvio.padrao))
+     xlim = c(media - 4*desvio.padrao, media + 3*4*desvio.padrao),
+     main = "FDP para a normal sua lognormal correspondente",
+     sub = paste("Normal com média", media, "e desvio padrão", desvio.padrao))
+
 lines(density(log.normal), col = "blue", lwd = 2, las = 1)
+
 legend("topright", c("Normal", "Lognormal"), col = c("red", "blue"), lwd = 2)
 dev.off()
 
+
 png(filename = "output-questao4-graficos-separados.png", width = 960, height = 480)
 par(mfrow = c(2,1))
+
 plot(density(normal), col = "red", las = 1,
      main = paste("Normal com média", media, "e desvio padrão", desvio.padrao))
+
 plot(density(log.normal), col = "blue", las = 1,
      main = paste("Lognormal gerada a partir da distribuição acima"))
+
 dev.off()

d5f5153e51268afa1560a6a81ba638625713442f
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2012-03-15 06:38:15 -0300

    script para resposta da questão 2

--- lab005/resposta/gabriel-lab05-questao2.R
+++ lab005/resposta/gabriel-lab05-questao2.R
@@ -1,3 +1,5 @@
+library("moments")
+
 arquivo.de.dados <- commandArgs(trailingOnly = TRUE)[1]
 
 dados <- read.table(file = arquivo.de.dados, header = TRUE)
@@ -8,39 +10,58 @@ dados <- read.table(file = arquivo.de.dados, header = TRUE)
 ## Escreva um script R que calcula o Zskewness e o Zkurtosis dos seguintes dados:
 
 # Zskewness
+z.skewness <- function(data)
+    skewness(data) / sqrt(6 / length(data))
+
 # Zkurtosis
+z.kurtosis <- function(data)
+    kurtosis(data) / sqrt(24 / length(data))
+
+
+linha.de.resultado.para <- function(cenario, dados)
+    data.frame(cenario = cenario,
+               Zskewness = c(z.skewness(dados)),
+               Zkurtosis = c(z.kurtosis(dados)))
+
 
 ## (i) intervalos em que as máquinas estiveram ocupadas;
 
 intervalos.ocupadas <- dados[dados$ociosa == FALSE, ]
 
-# Zskewness
-# Zkurtosis
+resultados.ocupadas <- linha.de.resultado.para("maquinas ocupadas", intervalos.ocupadas$intervalo)
 
 
 ## (ii) intervalos em que as máquinas estiveram ociosas;
 
 intervalos.ociosas <- dados[dados$ociosa == TRUE, ]
 
-# Zskewness
-# Zkurtosis
+resultados.ociosas <- linha.de.resultado.para("maquinas ociosas", intervalos.ociosas$intervalo)
 
 
 ## (iii) intervalos em que as máquinas estiveram ociosas separados por laboratório.
 
 numero.de.laboratorios = length(levels(dados$laboratorio))
 
-by(intervalos.ociosas[, c("intervalo", "laboratorio")],
-   list(laboratorio = intervalos.ociosas$laboratorio),
-   function(x) {
-       nome.do.laboratorio = levels(factor(x$laboratorio))[1]
+by.resultados.por.laboratorio <-
+    by(intervalos.ociosas[, c("intervalo", "laboratorio")],
+       list(laboratorio = intervalos.ociosas$laboratorio),
+       function(x) {
+           nome.do.laboratorio = levels(factor(x$laboratorio))[1]
 
-       # Zskewness
-       # Zkurtosis
-   })
+           linha.de.resultado.para(paste("máquinas ociosas de", nome.do.laboratorio),
+                                   x$intervalo)
+       })
+
+# converte by em data.frame
+resultados.por.laboratorio <- do.call(rbind, by.resultados.por.laboratorio)
+
 
 ## Os resultados dos três itens devem ser salvos como uma tabela um único
 ## arquivo no formato *.txt. A tabela deve ter 3 colunas: cenário, Zskewness e Zkurtosis .
 
-# write.table
+resultados <- rbind(resultados.ocupadas,
+                    resultados.ociosas,
+                    resultados.por.laboratorio)
+
+write.table(resultados, file = "output-questao2.txt")
 

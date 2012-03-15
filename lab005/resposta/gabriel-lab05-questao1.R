6a594d730e922b45113a1a7065d20561b286fec4
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2012-03-14 22:41:12 -0300

    Item i respondido

--- lab005/resposta/gabriel-lab05-questao1.R
+++ lab005/resposta/gabriel-lab05-questao1.R
@@ -9,48 +9,57 @@ dados <- read.table(file = arquivo.de.dados, header = TRUE)
 ## instruções abaixo:
 
 ## Escreva um script R que gera os gráficos FDA, FDP e as qqnorms dos seguintes
-## dados: (i) intervalos em que as máquinas estiveram ocupadas; (ii) intervalos em que
-## as máquinas estiveram ociosas; (iii) intervalos em que as máquinas estiveram ociosas
-## separados por laboratório. Nos gráficos FDPs, plote duas linhas, uma com a densidade
+## dados:
+## Nos gráficos FDPs, plote duas linhas, uma com a densidade
 ## dos dados obtidos na base de dados e outra com a densidade de uma distribuição
 ## normal gerada com a média e o desvio padrão dos dados obtidos da base de dados.
 ## Para cada item solicitado deve ser gerada uma figura no formato *.png, portanto, ao
 ## todo devem ser geradas 3 figuras.
 
-# TODO
-#
-# plotar gráficos em PNGs
-# revisar labels dos gráficos
-# revisar las = 1
-# intervalos.ociosas <- dados[dados$ociosa == TRUE, ]
-# intervalos.ociosas agrupados por laboratório
+## (i) intervalos em que as máquinas estiveram ocupadas;
 
 intervalos.ocupadas <- dados[dados$ociosa == FALSE, ]
 
 # FDA
-plot(ecdf(intervalos.ocupadas$intervalo),
-     main = "FDA para os intervalos em que as máquinas ficaram ocupadas",
-     xlab = "Tempo ocupada",
-     las = 1)
-
+fda.plot <- function() {
+    plot(ecdf(intervalos.ocupadas$intervalo),
+         main = "FDA para os intervalos em que as máquinas estiveram ocupadas",
+         xlab = "Tempo ocupada",
+         las = 1)
+}
 
 # FDP
 n = length(intervalos.ocupadas$intervalo)
 media = mean(intervalos.ocupadas$intervalo)
 desvio.padrao = sd(intervalos.ocupadas$intervalo)
 
-plot(density(intervalos.ocupadas$intervalo), col = "blue", lwd = 1,
-     main = "FDPs para os intervalos em que as máquinas ficaram ocupadas",
-     las = 1)
+fdp.plot <- function() {
+    plot(density(intervalos.ocupadas$intervalo), col = "blue", lwd = 1,
+         main = "FDPs para os intervalos em que as máquinas ficaram ocupadas",
+         las = 1)
 
-lines(density(rnorm(n, media, desvio.padrao)), col = "red", lwd = 1)
-legend("topright", c("Real", "Normal"), col = c("blue", "red"), lwd = 1)
-
+    lines(density(rnorm(n, media, desvio.padrao)), col = "red", lwd = 1)
+    legend("topright", c("Real", "Normal"), col = c("blue", "red"), lwd = 1)
+}
 
 # qqnorm
-qqnorm(intervalos.ocupadas$intervalo, las = 1)
-qqline(intervalos.ocupadas$intervalo)
+qqnorm.plot <- function() {
+    qqnorm(intervalos.ocupadas$intervalo, las = 1)
+    qqline(intervalos.ocupadas$intervalo)
+}
 
+# Figura
+png(filename = "output-questao1-i.png", width = 480, height = 3 * 480)
+par(mfrow = c(3,1))
+    fda.plot()
+    fdp.plot()
+    qqnorm.plot()
+dev.off()
+
+
+## (ii) intervalos em que as máquinas estiveram ociosas;
+
+## (iii) intervalos em que as máquinas estiveram ociosas separados por laboratório.
 
 
 ## Gere os gráficos utilizando o seu script. Escreva um relatório apresentando os gráficos

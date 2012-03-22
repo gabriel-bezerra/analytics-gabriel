d09d554a9946e034be1f85c4c12ecbc810521d9b
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2012-03-22 20:31:40 -0300

    Script resposta da questao 3: comparação das 3 amostras ao mesmo tempo

--- lab006/resposta/gabriel-lab06-questao3.R
+++ lab006/resposta/gabriel-lab06-questao3.R
@@ -1,3 +1,8 @@
+require("ggplot2")
+
+dados <- read.table(file = "velocidade-isp.txt", header = TRUE)
+
+dados.gzt <- dados[dados$provedor == "GZT", ]
 
 ## 3. Sidnelson também viu que no contrato da “GZT” eles dizem que a qualidade de serviço oferecida
 ## em todas regiões do país é a mesma para planos semelhantes, incluindo a velocidade de conexão.
@@ -12,4 +17,73 @@
 ## entre os estados?
 ## Caso seja identificada diferença de velocidade entre os estados, analise estatisticamente e
 ## identifique qual o estado possui a melhor conexão, discutindo como você chegou à sua conclusão.
+
+
+dados.gzt.pb <- dados.gzt[dados.gzt$estado == "PB", ]
+dados.gzt.sp <- dados.gzt[dados.gzt$estado == "SP", ]
+dados.gzt.rj <- dados.gzt[dados.gzt$estado == "RJ", ]
+
+source("intervalos-de-confianca.R")
+
+intervalo.de.confianca.pb =  intervalo.de.confianca.para.a.media(dados.gzt.pb$velocidade, 0.05)
+intervalo.de.confianca.sp =  intervalo.de.confianca.para.a.media(dados.gzt.sp$velocidade, 0.05)
+intervalo.de.confianca.rj =  intervalo.de.confianca.para.a.media(dados.gzt.rj$velocidade, 0.05)
+
+media = c(mean(dados.gzt.pb$velocidade),
+          mean(dados.gzt.sp$velocidade),
+          mean(dados.gzt.rj$velocidade))
+
+ics = rbind(intervalo.de.confianca.pb,
+            intervalo.de.confianca.sp,
+            intervalo.de.confianca.rj)
+
+media.icmin.icmax = data.frame(estado = c("PB", "SP", "RJ"),
+                               media = media,
+                               ic.min = ics[, 1],
+                               ic.max = ics[, 2])
+
+ggplot(media.icmin.icmax, aes(x = estado,
+                              y = media,
+                              ymin = ic.min,
+                              ymax = ic.max,
+                              fill = estado)) +
+    geom_bar() +
+    geom_errorbar(aes(width = 0.2)) +
+    opts(title = "Intervalos de confiança para a média de velocidade do GZT",
+         legend.position = "none") +
+    ylab("Média") +
+    xlab("Estado")
+
+
+# A comparação dos intervalos de confiança leva a crer que as médias de velocidades dos estados são diferentes, pois os
+# intervalos de SP e RJ  não se sobrepõem. Entretanto, para uma análise mais segura, realizamos o teste ANOVA -- o teste
+# mais adequado à situação de comparação da média de duas ou mais amostras independentes (pois foram coletadas em meses
+# diferentes).
+
+# Suas premissas são:
+#    Normalidade dos dados; e
+#    Homoscedasticidade.
+
+# Verificando as premissas...
+
+# ...Normalidade
+shapiro.test(dados.gzt.pb$velocidade)
+shapiro.test(dados.gzt.sp$velocidade)
+shapiro.test(dados.gzt.rj$velocidade)
+
+# ...Homoscedasticidade
+bartlett.test(list(dados.gzt.pb$velocidade,
+                   dados.gzt.sp$velocidade,
+                   dados.gzt.rj$velocidade))
+
+# Teste ANOVA
+valores.empilhados <- stack(data.frame(pb = dados.gzt.pb$velocidade,
+                                       sp = dados.gzt.sp$velocidade,
+                                       rj = dados.gzt.rj$velocidade))
+
+oneway.test(values ~ ind,
+            valores.empilhados,
+            var.equal = TRUE) # Como o teste de Bartlett forneceu um p-value alto (> 0.4),
+                              # podemos assumir que a variância é a mesma.
+
 

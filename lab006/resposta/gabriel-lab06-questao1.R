7e6ac0c148368245973e5e7c9e65c1a2522e9b87
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2012-03-22 19:30:47 -0300

    Script resposta da questao 1

--- lab006/resposta/gabriel-lab06-questao1.R
+++ lab006/resposta/gabriel-lab06-questao1.R
@@ -1,3 +1,7 @@
+require("ggplot2")
+
+dados <- read.table(file = "velocidade-isp.txt", header = TRUE)
+dados.pb <- dados[dados$estado == "PB", ]
 
 ## 1. Disseram a Sidnelson que na prática os dois provedores da Paraíba possuem, em média, a
 ## mesma velocidade de conexão para o plano de 50Mbps. Verifique se o que dizem realmente é
@@ -6,4 +10,61 @@
 ## Discuta se existe diferença estatística entre as médias de velocidade de conexão dos dois
 ## provedores. Caso exista diferença, qual provedor você indicaria a Sidnelson? Faça uma análise
 ## para diferentes níveis de significância.
+
+dados.pb.gzt <- dados.pb[dados.pb$provedor == "GZT", ]
+dados.pb.ola <- dados.pb[dados.pb$provedor == "Ola", ]
+
+source("intervalos-de-confianca.R")
+
+intervalo.de.confianca.gzt =  intervalo.de.confianca.para.a.media(dados.pb.gzt$velocidade, 0.05)
+intervalo.de.confianca.ola =  intervalo.de.confianca.para.a.media(dados.pb.ola$velocidade, 0.05)
+
+media = c(mean(dados.pb.gzt$velocidade),
+          mean(dados.pb.ola$velocidade))
+
+ics = rbind(intervalo.de.confianca.gzt,
+            intervalo.de.confianca.ola)
+
+media.icmin.icmax = data.frame(provedor = c("GZT", "Ola"),
+                               media = media,
+                               ic.min = ics[, 1],
+                               ic.max = ics[, 2])
+
+ggplot(media.icmin.icmax, aes(x = provedor,
+                              y = media,
+                              ymin = ic.min,
+                              ymax = ic.max,
+                              fill = provedor)) +
+    geom_bar() +
+    geom_errorbar(aes(width = 0.2)) +
+    opts(title = "Intervalos de confiança para a média de velocidade dos provedores",
+         legend.position = "none") +
+    ylab("Média") +
+    xlab("Provedor")
+
+
+# A comparação dos intervalos de confiança para as médias não foi conclusiva, há sobreposição de intervalos, porém
+# nenhuma das médias pertence ao intervalo de confiança da média da outra operadora. Sendo assim, não podemos concluir
+# que sejam diferentes nem que sejam iguais.
+
+# Para comparar as médias de duas amostras pareadas, como é o caso deste exercício, utiliza-se o teste t de Student.
+# Suas premissas são:
+#    Normalidade dos dados; e
+#    Homoscedasticidade.
+
+# Verificando as premissas...
+
+# ...Normalidade
+shapiro.test(dados.pb.gzt$velocidade)
+shapiro.test(dados.pb.ola$velocidade)
+
+# ...Homoscedasticidade
+bartlett.test(list(dados.pb.gzt$velocidade, dados.pb.ola$velocidade))
+
+
+# Teste de igualdade de médias
+t.test(dados.pb.gzt$velocidade, dados.pb.ola$velocidade,
+       paired = TRUE,
+       var.equal = TRUE) # Como o teste de Bartlett forneceu um p-value alto (> 0.7),
+                         # podemos assumir que a variância é a mesma.
 

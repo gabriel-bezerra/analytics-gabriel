26e1c7169cc8b0073b049d0e63be447708cc3f38
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2012-04-22 17:39:01 -0300

    script para gerar artefatos de entrega

---
+++ reconhecimento-de-padroes-e-redes-neurais/pack.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+LAB=$1
+LABXX=$(echo $LAB | tr '[:lower:]' '[:upper:]') # to upper case
+NOME=GABRIEL
+OUTPUT=gabriel-PRNN-$LABXX.zip
+
+if [ ! -d $LAB/resposta ]; then
+    echo $LAB must be a directory with a directory resposta inside
+    exit -1
+fi
+
+# ./
+echo Making temporary directory
+mkdir tmp
+cd tmp
+
+# ./tmp
+echo Making directory: $LABXX
+mkdir $LABXX
+cd $LABXX
+
+# ./tmp/$LABXX
+echo Making directory: $LABXX/$NOME
+ln -s ../../$LAB/resposta $NOME
+cd $NOME
+
+# ./tmp/$LABXX/$NOME
+echo Converting ODTs to PDF
+libreoffice --headless --convert-to pdf:writer_pdf_Export *.odt
+cd ../..
+
+# ./tmp/
+echo Creating package: $OUTPUT with all the '*.{R,txt,pdf,csv}' files
+zip $OUTPUT $LABXX/$NOME/*.{R,txt,pdf,csv}
+mv $OUTPUT ..
+cd ..
+
+# ./
+echo Cleaning up
+rm -r tmp
+
+echo Finished
+

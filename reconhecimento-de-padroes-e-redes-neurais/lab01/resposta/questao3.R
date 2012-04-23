c217249c1dcfb6cec9960893565435d44b822fe8
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2012-04-23 00:35:46 -0300

    3b

--- reconhecimento-de-padroes-e-redes-neurais/lab01/resposta/questao3.R
+++ reconhecimento-de-padroes-e-redes-neurais/lab01/resposta/questao3.R
@@ -17,21 +17,45 @@ classificacao <- function(padrao)
     )
 
 #FP, TP, FN, TN
-sapply(levels(dados$classe),
-       function(classe) {
-           TP <- classificacao(dados[dados$classe == classe, ]) == classe
-           FP <- classificacao(dados[dados$classe != classe, ]) == classe
-           TN <- classificacao(dados[dados$classe != classe, ]) != classe
-           FN <- classificacao(dados[dados$classe == classe, ]) != classe
+fp.tp.fn.tn <-
+    sapply(levels(dados$classe),
+           function(classe) {
+               TP <- classificacao(dados[dados$classe == classe, ]) == classe
+               FP <- classificacao(dados[dados$classe != classe, ]) == classe
+               TN <- classificacao(dados[dados$classe != classe, ]) != classe
+               FN <- classificacao(dados[dados$classe == classe, ]) != classe
 
-           P <- classificacao(dados[dados$classe == classe, ])
-           N <- classificacao(dados[dados$classe != classe, ])
+               P <- classificacao(dados[dados$classe == classe, ])
+               N <- classificacao(dados[dados$classe != classe, ])
 
-           list(TP = length(which(TP)),
-                FP = length(which(FP)),
-                TN = length(which(TN)),
-                FN = length(which(FN)),
-                TPR = length(which(TP)) / length(P),
-                FPR = length(which(FP)) / length(N))
-       })
+               list(TP = length(which(TP)),
+                    FP = length(which(FP)),
+                    TN = length(which(TN)),
+                    FN = length(which(FN)),
+                    TPR = length(which(TP)) / length(P),
+                    FPR = length(which(FP)) / length(N))
+           })
+
+print(fp.tp.fn.tn)
+
+# Precision, Recall, F-Measure
+prec.rec.f <-
+    sapply(levels(dados$classe),
+           function(classe) {
+               TP <- fp.tp.fn.tn[[c("TP"), classe]]
+               FP <- fp.tp.fn.tn[[c("FP"), classe]]
+
+               P <- length(classificacao(dados[dados$classe == classe, ]))
+
+               PREC <- TP / (TP + FP)
+               REC <- TP / P
+               F.Measure <- 2 / ((1/PREC) + (1/REC))
+
+               list(PREC = PREC,
+                    REC = REC,
+                    F.Measure = F.Measure)
+           })
+
+print(prec.rec.f)
+
 

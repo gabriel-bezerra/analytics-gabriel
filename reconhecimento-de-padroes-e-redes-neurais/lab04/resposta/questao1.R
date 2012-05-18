7a1d1420f2f6f161d3b9d8ce35884bebf156f18c
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2012-05-18 04:19:04 -0300

    script resposta da questao 1

--- reconhecimento-de-padroes-e-redes-neurais/lab04/resposta/questao1.R
+++ reconhecimento-de-padroes-e-redes-neurais/lab04/resposta/questao1.R
@@ -5,12 +5,26 @@ min.brutos <- min(dados.brutos)
 
 dados.normalizados <- (dados.brutos - min.brutos) / (max.brutos - min.brutos)
 
+shift <- function(x) {
+    l = nrow(x)
+    rbind(tail(x, l-1), c(NA))
+}
+
+mes1 <- dados.normalizados
+mes2 <- shift(mes1)
+mes3 <- shift(mes2)
+mes4 <- shift(mes3)
+
+dados.de.venda.com.nas = data.frame("mes1"=mes1[,1],
+                                    "mes2"=mes2[,1],
+                                    "mes3"=mes3[,1],
+                                    "mes4"=mes4[,1])
 
 
-# para visualizacao dos dados
+# Remove NAs no fim do arquivo
 
-plot.seq <- function(sequencia)
-    plot(data.frame(seq_len(nrow(sequencia)), sequencia), type = "l")
+tamanho.com.nas = nrow(dados.de.venda.com.nas)
+tamanho.sem.nas = tamanho.com.nas - 3
 
-plot.seq(dados.brutos)
-plot.seq(dados.normalizados)
+dados.de.venda <- head(dados.de.venda.com.nas, tamanho.sem.nas)
+
